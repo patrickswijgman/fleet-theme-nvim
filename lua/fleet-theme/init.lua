5d713b5c6bc44193bbff514e6035fe88fa737431
Patrick <petrik09@live.nl>
2024-11-16 10:41:39 +0100

    chore: formatting

--- lua/fleet-theme/init.lua
+++ lua/fleet-theme/init.lua
@@ -1,33 +1,33 @@
 local M = {}
 
 M.config = {
-    overrides = {},
+	overrides = {},
 }
 
 function M.setup(config)
-    M.config = vim.tbl_deep_extend("force", M.config, config or {})
+	M.config = vim.tbl_deep_extend("force", M.config, config or {})
 end
 
 function M.load()
-    if vim.version().minor < 8 then
-        vim.notify_once("fleet-theme.nvim: you must use neovim 0.8 or higher")
-        return
-    end
+	if vim.version().minor < 8 then
+		vim.notify_once("fleet-theme.nvim: you must use neovim 0.8 or higher")
+		return
+	end
 
-    -- reset colors
-    if vim.g.colors_name then
-        vim.cmd.hi("clear")
-    end
+	-- reset colors
+	if vim.g.colors_name then
+		vim.cmd.hi("clear")
+	end
 
-    vim.g.colors_name = "fleet"
-    vim.o.termguicolors = true
+	vim.g.colors_name = "fleet"
+	vim.o.termguicolors = true
 
-    local groups = require("fleet-theme.groups").setup()
+	local groups = require("fleet-theme.groups").setup()
 
-    -- add highlights
-    for group, settings in pairs(groups) do
-        vim.api.nvim_set_hl(0, group, settings)
-    end
+	-- add highlights
+	for group, settings in pairs(groups) do
+		vim.api.nvim_set_hl(0, group, settings)
+	end
 end
 
 return M
